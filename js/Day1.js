6bce64b024e59055bd516454e4f5384866494034
Dhanush Kumar <05dhanushkumar@gmail.com>
2025-08-11 20:48:29 +0530

    Day_1 js Code Added

---
+++ js/Day1.js
@@ -0,0 +1,59 @@
+console.log("dhanu");
+
+console.log("dhanu");
+console.log("kiru");
+console.log("loke");
+console.log("yeshu");
+console.log(a);
+
+why dynamic 
+var name = "dhanu";
+name=8;
+console.log(name);
+
+var 
+var a=5;
+var b=10;
+var c=12;
+console.log(c);
+
+let 
+{
+    let a=10;
+    a="ten";
+    console.log(a);
+}
+
+
+const 
+{
+    const c=20;
+    a="ten";
+    console.log(c);
+}
+
+pre increment 
+var a=10;
+console.log(++a);
+
+post increment
+var a=10;
+console.log(a++);
+console.log(a);
+
+data types 
+var a=6;
+console.log(typeof a);
+
+var a=true;
+var d;
+
+var user=null;
+console.log(user);
+const data = {
+    name:"dhanu",
+    age:"23",
+};
+user = data;
+
+console.log(user);
